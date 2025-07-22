91774a65493d8308f1e401cfa312e7789077bb13
Simon Bertram <simonbertram@hotmail.com>
2025-07-22 15:38:56 +0100

    Handle complex logic with useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 import "./App.css";
 import Navbar from "./components/navbar";
 import Card from "./components/card";
 import UploadForm from "./components/upload-form";
+
+interface Inputs {
+  title: string | null;
+  file: File | null;
+  path: string | null;
+}
+
+interface State {
+  items: string[];
+  count: number;
+  inputs: Inputs;
+  isCollapsed: boolean;
+}
+
+interface Action {
+  type: "setItem";
+  payload: {
+    path: string;
+  };
+}
 
 const photos = [
   "https://picsum.photos/id/1001/200/200",
@@ -15,6 +35,29 @@ const photos = [
   "https://picsum.photos/id/1009/200/200",
 ];
 
+const initialState: State = {
+  items: photos,
+  count: photos.length,
+  inputs: {
+    title: null,
+    file: null,
+    path: null,
+  },
+  isCollapsed: false,
+};
+
+function reducer(state: State, action: Action) {
+  switch (action.type) {
+    case "setItem":
+      return {
+        ...state,
+        items: [action.payload.path, ...state.items],
+      };
+    default:
+      return state;
+  }
+}
+
 function App() {
   const [inputs, setInputs] = useState<{
     title: string | null;
@@ -25,13 +68,8 @@ function App() {
     file: null,
     path: null,
   });
-  const [items, setItems] = useState(photos);
+  const [state, dispatch] = useReducer(reducer, initialState);
   const [isCollapsed, setIsCollapsed] = useState(false);
-  const [count, setCount] = useState<string>("");
-
-  useEffect(() => {
-    setCount(`You have ${items.length} image${items.length > 1 ? "s" : ""}`);
-  }, [items]);
 
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.name === "file" && e.target.files && e.target.files[0]) {
@@ -46,11 +84,26 @@ function App() {
   const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (inputs.path) {
-      setItems([inputs.path, ...items]);
+      // setItems([inputs.path, ...items]);
+      dispatch({ type: "setItem", payload: { path: inputs.path } });
+      setInputs({
+        title: null,
+        file: null,
+        path: null,
+      });
+      handleCollapse();
     }
   };
 
   const handleCollapse = () => setIsCollapsed(!isCollapsed);
+
+  const count = `You have ${state.items.length} image${
+    state.items.length > 1 ? "s" : ""
+  }`;
+
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
 
   return (
     <>
@@ -63,7 +116,7 @@ function App() {
             className="btn btn-success float-end"
             onClick={handleCollapse}
           >
-            {isCollapsed ? "Hide" : "Add"}
+            {isCollapsed ? "Close Upload Form" : "Add Image"}
           </button>
           <UploadForm
             inputs={inputs}
@@ -74,8 +127,8 @@ function App() {
           <h1 className="mb-5">Gallery</h1>
           <h3>Count: {count}</h3>
           <div className="row mt-5">
-            {items.map((photo) => (
-              <Card key={photo} src={photo} />
+            {state.items.map((photo, index) => (
+              <Card key={`${photo}-${index}`} src={photo} />
             ))}
           </div>
         </div>
--- src/components/upload-form.tsx
+++ src/components/upload-form.tsx
@@ -11,6 +11,22 @@ interface UploadFormProps {
   onSubmit: React.FormEventHandler<HTMLFormElement>;
 }
 
+const Preview = ({ path }: { path: string | null }) => {
+  return (
+    path && (
+      <div
+        className="rounded p-1 m-5"
+        style={{
+          width: "30%",
+          height: "300px",
+          backgroundImage: `url(${path})`,
+          backgroundSize: "cover",
+        }}
+      ></div>
+    )
+  );
+};
+
 export default function UploadForm({
   inputs,
   isVisible,
@@ -25,6 +41,7 @@ export default function UploadForm({
       <>
         <p className="display-6 text-center mb-3">Upload Stock Image</p>
         <div className="mb-5 d-flex align-items-center justify-content-center">
+          <Preview {...inputs} />
           <form onSubmit={onSubmit}>
             <div className="mb-3">
               <input
